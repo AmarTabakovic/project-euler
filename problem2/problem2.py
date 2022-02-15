be701e812d231f7b42bbda651804becdf387b7c6
Amar Tabakovic <amar.tabakovic@students.bfh.ch>
2022-02-15 17:07:23 +0100

    Solved problem 25 + improved problem 2

--- problem2/problem2.py
+++ problem2/problem2.py
@@ -1,16 +1,9 @@
-def fib(n):
-    if n == 1 or n == 0:
-        return n
-    return fib(n - 1) + fib(n - 2)
-
 result = 0
-max_val = 4000000
-temp = 0
-i = 0
-while temp <= max_val:
-    temp = fib(i)
-    if temp % 2 == 0:
-        result += temp
-    i += 1
+a = 1
+b = 1
+while a <= 4000000: 
+    if a % 2 == 0:
+        result += a
+    a, b = b, a + b
 
 print(result)
---
+++ problem25/problem25.py
@@ -0,0 +1,10 @@
+a = 1
+b = 1
+i = 1
+while True:
+    if len(str(a)) == 1000:
+        break
+    a, b = b, a + b
+    i += 1
+
+print(i)
