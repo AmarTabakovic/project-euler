756fdf7b3f084cf25c2ded7020c5913a163ac66b
Amar Tabakovic <amar.tabakovic@students.bfh.ch>
2022-02-20 20:51:41 +0100

    Solution for problem 34 (it's ugly)

---
+++ problem34/problem34.py
@@ -0,0 +1,18 @@
+def factorial(n):
+    i = 1;
+    for x in range(1, n + 1):
+        i *= x
+    return i
+
+max_val = 1000000
+i = 3 
+while i < max_val:
+    val_str = str(i)
+    sum_fac = 0
+    for c in val_str:
+        sum_fac += factorial(ord(c) - 48)
+   
+    if sum_fac == i:
+        print(i)
+
+    i += 1
