6a232e1660b5d847fa6cf87d92a303148379a3a3
Amar Tabakovic <amar.tabakovic@students.bfh.ch>
2022-03-10 00:25:05 +0100

    Problem 3 solved in a weird way

---
+++ problem3/problem3.py
@@ -0,0 +1,13 @@
+import math
+
+def is_prime(n):
+    for i in range(2, 1 +  math.floor(math.sqrt(n))):
+        if n % i == 0:
+            return False
+    return True
+
+n = 600851475143
+for i in range(2, 1 +  math.floor(math.sqrt(n))):
+    if n % i == 0:
+        if is_prime(i):
+            print(i)
