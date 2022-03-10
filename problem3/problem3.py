3c871611ec3c376f2b1317d1970f0a0cae23c7cc
Amar Tabakovic <amar.tabakovic@students.bfh.ch>
2022-03-10 12:53:47 +0100

    Print only the solution for problem 3

--- problem3/problem3.py
+++ problem3/problem3.py
@@ -7,7 +7,11 @@ def is_prime(n):
     return True
 
 n = 600851475143
+biggest = 0
 for i in range(2, 1 +  math.floor(math.sqrt(n))):
     if n % i == 0:
         if is_prime(i):
-            print(i)
+            if i > biggest:
+                biggest = i
+
+print(biggest)
