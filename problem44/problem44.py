ed3964f21627a78878adbc903bb8d47989ee837a
Amar Tabakovic <amar.tabakovic@students.bfh.ch>
2022-04-26 11:02:36 +0200

    Solved problem 44

---
+++ problem44/problem44.py
@@ -0,0 +1,15 @@
+def pentagonal(n):
+    return n * ((3 * n) - 1) / 2
+
+s = set()
+for i in range(1, 10000):
+    s.add(pentagonal(i))
+
+min_v = 9999999999999
+for i in s:
+    for j in s:
+        x = abs(j - i)
+        if (j - i) in s and (i + j) in s and x < min_v : 
+            min_v = x 
+
+print(int(min_v))
