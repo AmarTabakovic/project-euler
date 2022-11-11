0f1d620d24e8b086014386c691de8445530ed104
Amar Tabakovic <amar.tabakovic@students.bfh.ch>
2022-11-11 16:23:32 +0100

    Problem 10

--- problem-0010/main.cpp
+++ problem-0010/main.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 
-int main() { return 0; }
+bool is_prime(long val) {
+  for (long i = 2; i * i <= val; i++)
+    if (val % i == 0) return false;
+
+  return true;
+}
+
+int main() {
+  long largest_prime = 0;
+  long current = 2;
+  long sum = 0;
+  while (largest_prime < 2'000'000) {
+    if (is_prime(current)) {
+      sum += current;
+      largest_prime = current;
+    }
+    current++;
+  }
+
+  std::cout << sum - largest_prime << std::endl;
+
+  return 0;
+}
