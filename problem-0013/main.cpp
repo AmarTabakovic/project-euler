df47f652557309c3356e8c0821bfec5777de879a
Amar Tabakovic <amar.tabakovic@students.bfh.ch>
2022-11-12 15:20:44 +0100

    Improved solution

--- problem-0013/main.cpp
+++ problem-0013/main.cpp
@@ -1,23 +1,21 @@
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <boost/multiprecision/cpp_int.hpp>
 
 int main() {
   std::string line;
   std::ifstream myfile("numbers.txt");
 
-  boost::multiprecision::uint512_t sum = 0;
+  long long sum = 0;
 
   if (myfile.is_open()) {
     while (getline(myfile, line)) {
-      boost::multiprecision::uint512_t val(line); 
-      sum += val;
+      sum += std::stoull(line.substr(0, 15));
     }
     myfile.close();
   }
 
-  std::cout << sum.str().substr(0,10) << std::endl;
+  std::cout << std::to_string(sum).substr(0,10) << std::endl;
 
   return 0;
 }
