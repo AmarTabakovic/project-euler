1f2805ee51823e407b917c8928f94552c2605f41
Amar Tabakovic <amar.tabakovic@students.bfh.ch>
2022-11-13 03:17:03 +0100

    Simpler solution

--- problem-0021/main.cpp
+++ problem-0021/main.cpp
@@ -1,12 +1,11 @@
 #include <cmath>
 #include <iostream>
-#include <map>
 
 const int N = 10'000;
 
 int get_sum_of_divisors(int n) {
   int sum = 0;
-  
+
   for (int i = 1; i <= n / 2; i++)
     if (n % i == 0) sum += i;
 
@@ -14,15 +13,12 @@ int get_sum_of_divisors(int n) {
 }
 
 int main() {
-  std::map<int, int> amicable_table;
-
-  for (int i = 1; i < N + 1; i++) amicable_table[i] = get_sum_of_divisors(i);
-
   int sum = 0;
 
-  for (int i = 1; i < N + 1; i++)
-    if (i == amicable_table[amicable_table[i]] && i != amicable_table[i])
-      sum += i;
+  for (int i = 1; i < N + 1; i++){
+    int b = get_sum_of_divisors(i);
+    if (i == get_sum_of_divisors(b) && i != b) sum += i;
+  }
 
   std::cout << sum << std::endl;
 
